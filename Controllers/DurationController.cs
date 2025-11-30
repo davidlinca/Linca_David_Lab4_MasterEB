8eba00421a43ab64c7152e93b972859ded5c31c5
David Linca <alexlinca_21@yahoo.ro>
2025-11-30 21:02:44 +0200

    Lab exercise

---
+++ Controllers/DurationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.ML;
+using static Linca_David_Lab4_MasterEB.DurationPredictionModel;
+
+namespace Linca_David_Lab4_MasterEB.Controllers
+{
+    public class DurationController : Controller
+    {
+        public IActionResult Time(ModelInput input)
+        {
+            MLContext mlContext = new MLContext();
+
+            ITransformer mlModel =
+                mlContext.Model.Load(@".\DurationPredictionModel.mlnet", out var modelInputSchema);
+
+            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput,
+                ModelOutput>(mlModel);
+
+            ModelOutput result = predEngine.Predict(input);
+
+            ViewBag.Duration = result.Score;
+
+            return View(input);
+        }
+    }
+}
